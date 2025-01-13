e4723a73360c2b41ef8a303fb79c7a5fb2d2931b
Shuang <57366173+Shuang-Plum@users.noreply.github.com>
2025-01-13 13:53:48 -0500

    Add files via upload

--- Fig5/XAK_structure.R
+++ Fig5/XAK_structure.R
@@ -198,6 +198,130 @@ p<-ggplot(data=a_k,aes(x=pos1,y=pos2,group=xfeature))+
 ggsave("XAK_shared_AK_structure_xfeature.svg", plot = p, width = 5.5, height = 5.5, units = "in")
 
 
+############################################################
+
+#############################################
+# plot XK chunks, no need to be shared with A for Fig S3 F
+
+x<-read.csv('mXist_XAK_q975chunk.csv',header=T)
+k<-read.csv('kcnq1ot1_XAK_q975chunk.csv',header=T)
+
+# add seq number
+x$pos<-c(1:nrow(x))
+k$pos<-c(1:nrow(k))
+
+colnames(x)[1]<-'xak_chunk'
+colnames(k)[1]<-'xak_chunk'
+
+
+x_xk<-x[(grepl('kcnq1ot1',x$sig_chunk)),]
+k_xk<-k[(grepl('mXist',k$sig_chunk)),]
+
+x_xk$xist_seq<-NULL
+x_xk$sig_seq<-NULL
+
+k_xk$k_seq<-NULL
+k_xk$sig_seq<-NULL
+
+allchunks<-rbind(x[,c(1,5)],k[,c(1,5)])
+
+# add xist feature color to denote repeat or interval
+xistfeatures<-c('mXist_repeatA','mXist_ss234','mXist_repeatFdwn','mXist_repeatB',
+                'mXist_repeatC','mXist_repeatEbroad')
+
+x_xk$xfeature<-''
+
+for (n in 1:nrow(x_xk)) {
+  if (x_xk$xak_chunk[n] %in% xistfeatures) {
+    x_xk$xfeature[n]<-'repeats'
+  } else {
+    x_xk$xfeature[n]<-'intervals'
+  }
+}
+
+
+shared_chunks<-function(df) {
+  
+  tdf<-data.frame(gene1=character(),
+                  chunk1=character(),
+                  pos1=integer(),
+                  gene2=character(),
+                  chunk2=character(),
+                  pos2=integer(),
+                  xfeature=character())
+  
+  for (n in 1:nrow(df)) {
+    
+    temp<-df$sig_chunk[n]
+    
+    temp<-unlist(strsplit(temp,';',fixed=T))
+    
+    temp<-gsub("\\(.*$", "", temp, perl = TRUE)
+    
+    
+    for (m in 1:length(temp)){
+      
+      tempg<-temp[m]
+      
+      tidx<-which(allchunks$xak_chunk==tempg)
+      
+      if(length(tidx)==1) {
+        tempdf<-data.frame(gene1=gsub("\\_.*$", "", df$xak_chunk[n], perl = TRUE),
+                           chunk1=df$xak_chunk[n],
+                           pos1=df$pos[n],
+                           gene2=gsub("\\_.*$", "", tempg, perl = TRUE),
+                           chunk2=tempg,
+                           pos2=allchunks$pos[tidx],
+                           xfeature=df$xfeature[n])
+        
+        tdf<-rbind(tdf,tempdf)
+        
+      } else {
+        print(n)
+        print(tempg)
+      }
+      
+    }
+    
+    
+  }
+  
+  return(tdf)
+  
+}
+
+
+x_xk_plot<-shared_chunks(x_xk)
+x_xk_plot<-x_xk_plot[which(x_xk_plot$gene2=='kcnq1ot1'),]
+
+x_xk_repeats<-x_xk_plot[which(x_xk_plot$xfeature=='repeats'),]
+
+cor.test(x_xk_plot$pos1, x_xk_plot$pos2, method = "kendall",alternative = "greater")
+
+cor.test(x_xk_repeats$pos1, x_xk_repeats$pos2, method = "kendall",alternative = "greater")
+
+p<-ggplot(data=x_xk_plot,aes(x=pos1,y=pos2,group=xfeature))+
+  #geom_point(fill='#7fbf7b',color='#1b7837', alpha=0.6,size=4) +
+  geom_point(aes(fill=xfeature,color=xfeature), alpha=0.6,size=6) +
+  labs(x = "Xist",y = "Kcnq1ot1") +
+  #coord_cartesian(xlim=c(0,500000))+
+  scale_x_continuous(breaks=seq(from=0, to=31,by=10))+
+  scale_y_continuous(breaks=seq(from=0, to=190,by=40))+
+  scale_color_manual(values=c('#762a83','#1b7837'))+
+  scale_fill_manual(values=c('#af8dc3','#7fbf7b'))+
+  theme(panel.background=element_rect(fill='white'),
+        plot.margin = margin(10, 15, 10, 10, "pt"),
+        # panel.grid.major=element_line(color='grey',linewidth =0.3),
+        panel.grid.major=element_blank(),
+        axis.line.x = element_line(color="black", linewidth = 0.5),
+        axis.line.y = element_line(color="black", linewidth = 0.5),
+        legend.position='none',
+        axis.title.x = element_text(size = 28),
+        axis.title.y = element_text(size = 28),
+        axis.text.x=element_text(size=28),
+        axis.text.y=element_text(size=28))
+
+ggsave("XK_shared_XK_structure_xfeature.svg", plot = p, width = 5.5, height = 5.5, units = "in")
 
 
 
